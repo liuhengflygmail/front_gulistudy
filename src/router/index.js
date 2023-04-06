8d92c52492d1b296da244a6f124d4abcb661cf9f
liuheng <liuhengfly@gmail.com>
2023-04-06 22:54:32 +0800

    添加课程路由：添加隐藏路由，做页面跳转

--- src/router/index.js
+++ src/router/index.js
@@ -87,28 +87,49 @@ export const constantRouterMap = [
       }
     ]
   },
-    // 课程管理路由
-    {
-      path: '/course',
-      component: Layout,
-      redirect: 'course/list',
-      name: '课程管理',
-      meta: { title: '课程分类管理', icon: 'nested' },
-      children: [
-        {
-          path: 'list',
-          name: '课程列表',
-          component: () => import('@/views/edu/subject/list'),
-          meta: { title: '课程列表', icon: 'table' }
-        },
-        {
-          path: 'save',
-          name: '添加课程',
-          component: () => import('@/views/edu/subject/save'),
-          meta: { title: '添加课程', icon: 'nested' }
-        }
-      ]
-    },
+  // 课程管理
+  {
+    path: '/edu/course',
+    component: Layout,
+    redirect: '/edu/course/list',
+    name: 'Course',
+    meta: { title: '课程管理', icon: 'form' },
+    children: [
+      {
+        path: 'list',
+        name: 'EduCourseList',
+        component: () => import('@/views/edu/course/list'),
+        meta: { title: '课程列表' }
+      },
+      {
+        path: 'info',
+        name: 'EduCourseInfo',
+        component: () => import('@/views/edu/course/info'),
+        meta: { title: '发布课程' }
+      },
+      {
+        path: 'info/:id',
+        name: 'EduCourseInfoEdit',
+        component: () => import('@/views/edu/course/info'),
+        meta: { title: '编辑课程基本信息', noCache: true },
+        hidden: true
+      },
+      {
+        path: 'chapter/:id',
+        name: 'EduCourseChapterEdit',
+        component: () => import('@/views/edu/course/chapter'),
+        meta: { title: '编辑课程大纲', noCache: true },
+        hidden: true
+      },
+      {
+        path: 'publish/:id',
+        name: 'EduCoursePublishEdit',
+        component: () => import('@/views/edu/course/publish'),
+        meta: { title: '发布课程', noCache: true },
+        hidden: true
+      }
+    ]
+  },
   {
     path: '/example',
     component: Layout,
