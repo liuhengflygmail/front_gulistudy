90af19388c1452d246b865a989266b5c959a8bdd
liuheng <liuhengfly@gmail.com>
2023-04-12 21:25:08 +0800

    课程封面上传

--- src/views/edu/course/info.vue
+++ src/views/edu/course/info.vue
@@ -43,6 +43,16 @@
       </el-form-item>
 
       <!-- 课程封面 TODO -->
+      <el-form-item label="课程封面">
+        <el-upload
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+          :action="BASE_API + '/eduoss/fileoss/upload'"
+          class="avatar-uploader">
+          <img :src="courseInfo.cover" >
+        </el-upload>
+      </el-form-item>
 
       <el-form-item label="课程价格">
         <el-input-number :min="0" v-model="courseInfo.price" controls-position="right" placeholder="免费课程请设置为0元" /> 元
@@ -61,6 +71,7 @@ import subject from '@/api/edu/subject'
 export default {
   data() {
     return {
+      BASE_API: process.env.BASE_API, // 接口API地址
       saveBtnDisabled: false,
       courseInfo: {
         title: '',
@@ -69,7 +80,7 @@ export default {
         teacherId: '',
         lessonNum: 0,
         description: '',
-        cover: '',
+        cover: '/static/6.jpg',
         price: 0
       },
       teacherList: [], // 封装所有的讲师
@@ -84,6 +95,22 @@ export default {
     this.getOneSubject()
   },
   methods: {
+    // 上传封面成功调用的方法
+    handleAvatarSuccess(res, file) {
+      this.courseInfo.cover = res.data.url
+    },
+    // 上传之前调用的方法
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
+    },
     // 点击某个一级分类,触发change,显示对应二级分类
     subjectLevelOneChanged(value) {
       // value就是一级分类的id值
